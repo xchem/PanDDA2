88e0ae87e5b75c4a2a6350065ef374cce259e9f0
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2025-08-07 15:16:32 +0100

    Real space smoothing

--- pandda_gemmi/comparators/get_comparators.py
+++ pandda_gemmi/comparators/get_comparators.py
@@ -1,10 +1,11 @@
 from ..interfaces import *
 
 def get_comparators(datasets: Dict[str, DatasetInterface], filters, debug=False):
+    print(f'comparators : before : {len([x for x in datasets])}')
 
     for filter in filters:
         datasets = filter(datasets)
         if debug:
-            print(f'{type(filter)} : {[x for x in datasets]}')
+            print(f'comparators : {type(filter)} : {len([x for x in datasets])}')
 
     return datasets
