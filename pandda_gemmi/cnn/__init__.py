9b6fc4c8d3c0d145dbf95f2f58aa399cb9e135c8
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2026-05-07 13:45:47 +0100

    Residue based site model

--- pandda_gemmi/cnn/__init__.py
+++ pandda_gemmi/cnn/__init__.py
@@ -1,4 +1,4 @@
-# from pandda_gemmi.cnn.base import load_model_from_checkpoint, set_structure_mean, copy_map
-# from pandda_gemmi.cnn.resnets import resnet18, resnet10
-# from pandda_gemmi.cnn.build import BuildScorer, LitBuildScoring
-# from pandda_gemmi.cnn.event import EventScorer, LitEventScoring, Event
+from pandda_gemmi.cnn.build import BuildScorer, LitBuildScoring
+from pandda_gemmi.cnn.event import EventScorer, LitEventScoring, Event
+from pandda_gemmi.cnn.base import load_model_from_checkpoint, set_structure_mean, copy_map
+from pandda_gemmi.cnn.resnets import resnet18, resnet10
