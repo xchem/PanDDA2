9007ca05c372d690340c6a26d8e1527fa5e747cf
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2026-06-25 18:00:06 +0100

    Support for titration scoring

--- scripts/analyse_titration_series.py
+++ scripts/analyse_titration_series.py
@@ -78,9 +78,7 @@ def get_datasets(args, input_yaml):
     }
 
     datasets_to_process = {
-        series_name: input_yaml['series'][series_name][
-            max(input_yaml['series'][series_name], key=lambda _dtag: input_yaml['series'][series_name][_dtag])
-            ]
+        series_name: max(input_yaml['series'][series_name], key=lambda _dtag: input_yaml['series'][series_name][_dtag])
         for series_name
         in input_yaml['series']
     }
