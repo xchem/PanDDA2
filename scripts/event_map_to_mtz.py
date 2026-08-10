a84d165438e7c92582983df332bcb19353168b77
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2026-08-10 11:05:36 +0100

    Override sites support and site overclustering fix

--- scripts/event_map_to_mtz.py
+++ scripts/event_map_to_mtz.py
@@ -1,5 +1,6 @@
 import gemmi
 import fire
+import pathlib
 
 
 def event_map_to_mtz(event_map_path, reference_structure_path, output_mtz_path):
@@ -26,5 +27,30 @@ def event_map_to_mtz(event_map_path, reference_structure_path, output_mtz_path):
     mtz.write_to_file(str(output_mtz_path))
 
 
+def event_maps_to_mtz(pandda_path, model_building_path):
+    print(f'Making mtzs of event maps from {pandda_path} in {model_building_path}')
+    pandda_path = pathlib.Path(pandda_path)
+    for dataset_dir in (pandda_path / 'processed_datasets').glob('*'):
+        print(f'Processing {dataset_dir}')
+        if not dataset_dir.is_dir():
+            print(f'\tNot a directory, skipping!')
+            continue
+
+        dtag = dataset_dir.name
+
+        pdb_path = dataset_dir / f"{dtag}-pandda-input.pdb"
+
+        if not pdb_path.exists():
+            print(f'\tNo reference pdb {pdb_path}, skipping!')
+            continue
+
+        # Get the event maps
+        for event_map_path in dataset_dir.glob(f'{dtag}-event_*'):
+            event_mtz_path = model_building_path / f'{dtag}' / f"{event_map_path.stem}.mtz"
+            print(f'\tMaking event map mtz for {event_map_path.name} at {event_mtz_path}')
+            event_map_to_mtz(event_map_path, pdb_path, event_mtz_path)
+
+    print(f'Processed!')
+
 if __name__ == "__main__":
-    fire.Fire(event_map_to_mtz)
+    fire.Fire()
